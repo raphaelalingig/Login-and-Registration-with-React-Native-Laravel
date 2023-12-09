5e72db4eeac17887ed87641e280b48a76a5a82dd
RaizuuX <124657190+raphaelalingig@users.noreply.github.com>
2023-12-09 19:54:26 +0800

    Update SignupForm.js

--- components/screens/SignupForm.js
+++ components/screens/SignupForm.js
@@ -35,7 +35,7 @@ const SignupForm = () => {
         return false;
       }
 
-      const url = "http://192.168.1.15/api/v1/register";
+      const url = "http://192.168.1.15:8000/api/v1/register";
       const data = {
         name,
         email,
